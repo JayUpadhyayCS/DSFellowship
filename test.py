5ddd9db7eebbca663d17394baf17b2b5e0715259
jupadhyay1 <jupadhyay1@yahoo.com>
2022-07-07 01:01:23 -0700

    made plot

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 import pandas as pd
-
-data_xls = pd.read_excel('InSAR_data_south/20190122.xlsx', 'Sheet2', index_col=None)
-data_xls.to_csv('csvfile.csv', encoding='utf-8', index=False)
+excelPath='InSAR_data_south/20190122.xlsx'
+csvPath='InSAR_data_south/CSV/'
+data_xls = pd.read_excel(excelPath, 'Sheet2', index_col=None)
+data_xls.to_csv(csvPath+'csvfile.csv', encoding='utf-8', index=False)
+pd.
