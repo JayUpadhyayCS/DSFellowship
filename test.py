afd0846ff20ddd2c69c99a0c79b5966e9333f32f
jupadhyay1 <jupadhyay1@yahoo.com>
2022-08-11 01:46:28 -0700

    Can read from groundwater now

--- test.py
+++ test.py
@@ -1,6 +1,61 @@
-import pandas as pd
-excelPath='InSAR_data_south/20190122.xlsx'
-csvPath='InSAR_data_south/CSV/'
-data_xls = pd.read_excel(excelPath, 'Sheet2', index_col=None)
-data_xls.to_csv(csvPath+'csvfile.csv', encoding='utf-8', index=False)
-pd.
+# Tensorflow / Keras
+from tensorflow import keras # for building Neural Networks
+print('Tensorflow/Keras: %s' % keras.__version__) # print version
+from keras.models import Sequential # for creating a linear stack of layers for our Neural Network
+from keras import Input # for instantiating a keras tensor
+from keras.layers import Masking,Bidirectional, LSTM, RepeatVector, Dense, TimeDistributed,MaxPooling1D, Flatten, Conv1D,Conv2D,Dropout, MaxPooling2D # for creating layers inside the Neural Network
+from keras.optimizers import Adam , SGD
+# Data manipulation
+import pandas as pd # for data manipulation
+print('pandas: %s' % pd.__version__) # print version
+import numpy as np # for data manipulation
+print('numpy: %s' % np.__version__) # print version
+import numpy.ma as ma
+# Sklearn
+import sklearn
+print('sklearn: %s' % sklearn.__version__) # print version
+from sklearn.preprocessing import MinMaxScaler # for feature scaling
+from sklearn.model_selection import train_test_split
+# Visualization
+import plotly 
+import plotly.express as px
+import plotly.graph_objects as go
+print('plotly: %s' % plotly.__version__) # print version
+from matplotlib import pyplot as plt
+
+#file accessing
+import os
+# time stuff
+from datetime import timedelta
+import calendar
+df=pd.read_csv('InSAR_data_south/displacement/export_dataframe1.csv')
+df=df.set_index([df.columns[0],df.columns[1]])
+df.columns=pd.to_datetime(df.columns, format='%Y%m%d')
+df=df.dropna(axis=0, how='all')#drop full nan rows
+
+df=df.iloc[200000:2000100]
+print(0)
+df2=pd.read_csv('InSAR_data_south/displacement/groundwater.csv')
+df2=df2.set_index([df2.columns[0],df2.columns[1]])
+df2.columns=pd.to_datetime(df2.columns, format='%Y-%m-%d')
+df2=df2.dropna(axis=0, how='all')#drop full nan rows
+for y in range(df.columns.size):# includes all dates and adds nans
+    if df.columns[y] != df2.columns[y]: #if column is misssing insert it
+        df2.insert(loc=y,column=df.columns[y],value=np.nan*11286,allow_duplicates=False)
+df2
+print(1)
+
+for x in range(df2.index.size): #for each row
+    curr=np.nan
+    for y in range(df2.columns.size): #for each value in the row
+        n=0
+        while np.isnan(curr): # if saved value is empty
+            curr=df2.iloc[x].values[n] #look for next actual value
+            n+=1
+        if np.isnan(df2.iloc[x].values[y]):#if data is nan then replace with previous curr value
+            df2.iat[x,y]=curr
+        else:
+            curr= df2.iloc[x].values[y]
+                
+print(2)
+df2
